5c1d9cd2bf89946c515e5dc608501ff6210b858e
jikuixie <jikuixie@gmail.com>
2024-04-30 15:12:59 +0800

    228 nums AC

---
+++ Linked_List/228.summary-ranges.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=228 lang=python3
+# @lcpr version=30122
+#
+# [228] 汇总区间
+#
+
+
+# @lcpr-template-start
+from typing import *
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [0,1,2,4,5,7]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0,2,3,4,6,8,9]\n
+# @lcpr case=end
+
+#
+
