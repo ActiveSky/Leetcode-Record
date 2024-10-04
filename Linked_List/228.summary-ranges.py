402b30ffdafb8abd8f00a99f50f40521e92cc0b6
jikuixie <jikuixie@gmail.com>
2024-10-04 21:14:12 +0800

    modify two files

--- Linked_List/228.summary-ranges.py
+++ Linked_List/228.summary-ranges.py
@@ -10,8 +10,36 @@
 from typing import *
 # @lcpr-template-end
 # @lc code=start
+
+
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        ranges = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            # Check if the current number is not consecutive
+            if nums[i] != nums[i - 1] + 1:
+                # If we have a range, format it accordingly
+                if start == nums[i - 1]:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(f"{start}->{nums[i - 1]}")
+                # Update start to the current number
+                start = nums[i]
+
+        # Handle the last range or number
+        if start == nums[-1]:
+            ranges.append(str(start))
+        else:
+            ranges.append(f"{start}->{nums[-1]}")
+
+        return ranges
+
+        
 # @lc code=end
 
 
